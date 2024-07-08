a35fe566e887b66e0fb683fd1531ceae12c7d9f6
ShankarAswal <shankaraswal@gmail.com>
2024-07-08 13:51:46 +0100

    added: code comments in user.controller using cody (#12)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,9 @@ import ApiResponse from "../utils/ApiResponse.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js";
 
+// ---------------------------------------------------------------
 // GENERATE REFRESH TOKEN AND ACCESS TOKEN FN
+// ---------------------------------------------------------------
 /**
  * Generates an access token and a refresh token for a user.
  *
@@ -64,7 +66,9 @@ const generateTokens = async (userId) => {
   }
 };
 
+// ---------------------------------------------------------------
 // REGISTER USER
+// ---------------------------------------------------------------
 /**
  * Registers a new user by creating a new user account in the database.
  *
@@ -94,33 +98,112 @@ const registerUser = asyncHandler(async (req, res) => {
   // if created then send response
   // if not created then send error response
 
+  /**
+   * Extracts the user data from the request body.
+   *
+   * @param {Object} req - The Express request object.
+   * @param {string} req.body.userName - The username of the new user.
+   * @param {string} req.body.fullName - The full name of the new user.
+   * @param {string} req.body.email - The email address of the new user.
+   * @param {string} req.body.password - The password of the new user.
+   */
   const { userName, fullName, email, password } = req.body;
 
+  /**
+   * Validates that all required user data fields are provided.
+   *
+   * @param {string} fullName - The full name of the user.
+   * @param {string} userName - The username of the user.
+   * @param {string} email - The email address of the user.
+   * @param {string} password - The password of the user.
+   * @throws {ApiError} - If any of the required fields are empty or contain only whitespace.
+   */
   if (
     [fullName, userName, email, password].some((item) => item.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
+  /**
+   * Checks if a user with the given username or email already exists in the database.
+   *
+   * @param {string} userName - The username to check for.
+   * @param {string} email - The email address to check for.
+   * @returns {Promise<boolean>} - True if a user with the given username or email exists, false otherwise.
+   */
   const isUserExist = await User.findOne({
     $or: [{ userName: userName }, { email: email }],
   });
 
+  /**
+   * Checks if a user with the given username or email already exists in the database.
+   *
+   * @param {string} userName - The username to check for.
+   * @param {string} email - The email address to check for.
+   * @throws {ApiError} - If a user with the given username or email already exists.
+   */
   if (isUserExist) throw new ApiError(400, "User already exist");
 
+  /**
+   * Extracts the path of the avatar image file from the request files.
+   *
+   * @param {Object} req - The Express request object.
+   * @param {Object} req.files - The uploaded files attached to the request.
+   * @param {Object} req.files.avatarImage - The uploaded avatar image file.
+   * @param {string} req.files.avatarImage[0].path - The local file path of the uploaded avatar image.
+   */
   const avatarLocalPath = req.files?.avatarImage[0].path;
+
+  /**
+   * Extracts the path of the cover image file from the request files.
+   *
+   * @param {Object} req - The Express request object.
+   * @param {Object} req.files - The uploaded files attached to the request.
+   * @param {Object} req.files.coverImage - The uploaded cover image file.
+   * @param {string} req.files.coverImage[0].path - The local file path of the uploaded cover image.
+   */
   const coverImageLocalPath = req.files?.coverImage[0].path;
+
+  /**
+   * Checks if both the avatar and cover image local paths are empty, and throws an API error if so.
+   *
+   * @throws {ApiError} - If both the avatar and cover image local paths are empty, with a 400 status code and the message "Profile images are required".
+   */
   if (!avatarLocalPath && !coverImageLocalPath) {
     throw new ApiError(400, "Profile images are required");
   }
 
+  /**
+   * Uploads the avatar image to Cloudinary and returns the uploaded image URL.
+   *
+   * @param {string} avatarLocalPath - The local file path of the uploaded avatar image.
+   * @returns {Promise<{ url: string }>} - The uploaded avatar image URL.
+   */
   const avatarImage = await uploadOnCloudinary(avatarLocalPath);
+
+  /**
+   * Uploads the cover image to Cloudinary and returns the uploaded image URL.
+   *
+   * @param {string} coverImageLocalPath - The local file path of the uploaded cover image.
+   * @returns {Promise<{ url: string }>} - The uploaded cover image URL.
+   */
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
   if (!avatarImage && !coverImage) {
     throw new ApiError(400, "Profile images are required");
   }
 
   // DATABASE ENTRY
+  /**
+   * Creates a new user in the database with the provided user information.
+   *
+   * @param {Object} fullName - The full name of the user.
+   * @param {string} userName - The username of the user, converted to lowercase.
+   * @param {string} email - The email address of the user.
+   * @param {string} password - The password of the user.
+   * @param {string} avatarImage.url - The URL of the uploaded avatar image.
+   * @param {string} coverImage.url - The URL of the uploaded cover image.
+   * @returns {Promise<User>} - The newly created user object.
+   */
   const user = await User.create({
     fullName,
     userName: userName.toLowerCase(),
@@ -130,9 +213,19 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage.url,
   });
 
+  /**
+   * Retrieves the newly created user from the database, excluding the password and refreshToken fields.
+   *
+   * @returns {Promise<User>} - The newly created user object, with the password and refreshToken fields excluded.
+   */
   const isUserCreated = await User.findById(user._id).select(
     "-password -refreshToken"
   );
+
+  /**
+   * Throws an `ApiError` with a 500 status code and the message "User not created" if the `isUserCreated` variable is falsy.
+   * This is likely used as a safeguard to ensure that a user was successfully created in the database before proceeding with the response.
+   */
   if (!isUserCreated) throw new ApiError(500, "User not created");
 
   return res.status(201).json(
@@ -143,7 +236,9 @@ const registerUser = asyncHandler(async (req, res) => {
   );
 });
 
+// ---------------------------------------------------------------
 // LOGIN USER
+// ---------------------------------------------------------------
 /**
  * Logs in a user by verifying their credentials and generating access and refresh tokens.
  *
@@ -268,7 +363,9 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
+// ---------------------------------------------------------------
 // LOGOUT USER
+// ---------------------------------------------------------------
 /**
  * Logs out the currently authenticated user by removing the refresh token from the user's record and clearing the access and refresh token cookies.
  *
@@ -321,34 +418,111 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
+// ---------------------------------------------------------------
 // REFRESH ACCESS TOKEN FOR SESSION RENEWAL
+// ---------------------------------------------------------------
+/**
+ * Refreshes the access token for the current user session.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {string} req?.cookies?.refreshToken - The refresh token from the request cookies.
+ * @returns {Promise<ApiResponse>} - A successful API response containing the refreshed access and refresh tokens.
+ * @throws {ApiError} - If the refresh token is missing, invalid, or expired.
+ */
 const refreshAccessToken = asyncHandler(async (req, res) => {
+  /**
+   * Retrieves the refresh token from the request cookies.
+   *
+   * @param {Object} req - The Express request object.
+   * @param {string} req?.cookies?.refreshToken - The refresh token from the request cookies.
+   * @returns {string|undefined} - The refresh token if present, otherwise undefined.
+   */
   const newRefreshToken = req?.cookies?.refreshToken;
-  console.log("4456465456", req?.cookies);
+
+  /**
+   * Checks if a refresh token is present in the request. If not, throws an unauthorized error.
+   *
+   * @param {Object} req - The Express request object.
+   * @throws {ApiError} - If the refresh token is missing, throws an unauthorized error with a status code of 401.
+   */
   if (!newRefreshToken) {
     throw new ApiError(401, "Unauthorized request found");
   }
 
   try {
+    /**
+     * Verifies the provided refresh token and decodes the token to retrieve the user ID.
+     *
+     * @param {string} newRefreshToken - The refresh token to be verified.
+     * @returns {Object} - The decoded token payload, which includes the user ID.
+     * @throws {ApiError} - If the refresh token is invalid or expired.
+     */
     const decodeToken = jwt.verify(
       newRefreshToken,
       process.env.REFRESH_TOKEN_SECRET
     );
 
+    /**
+     * Retrieves the user document from the database based on the decoded token's user ID.
+     *
+     * @param {string} decodeToken._id - The user ID extracted from the decoded refresh token.
+     * @returns {Promise<User>} - The user document retrieved from the database.
+     * @throws {ApiError} - If the user is not found in the database.
+     */
     const userId = await User.findById(decodeToken._id);
+
+    /**
+     * Checks if a valid user ID was found in the decoded refresh token. If not, throws an unauthorized error.
+     *
+     * @param {Object} userId - The user document retrieved from the database based on the decoded refresh token.
+     * @throws {ApiError} - If the user ID is not found, throws an unauthorized error with a status code of 401.
+     */
     if (!userId) {
       throw new ApiError(401, "Invalid token found");
     }
 
+    /**
+     * Checks if the provided refresh token matches the user's current refresh token. If not, throws an unauthorized error with a status code of 401.
+     *
+     * @param {string} newRefreshToken - The refresh token from the request.
+     * @param {string} userId.refreshToken - The user's current refresh token stored in the database.
+     * @throws {ApiError} - If the refresh token is expired, throws an unauthorized error with a status code of 401.
+     */
     if (newRefreshToken !== userId?.refreshToken) {
       throw new ApiError(401, "Refresh token is expired");
     }
+
+    /**
+     * Options for setting the access and refresh tokens as cookies.
+     * These options specify that the cookies should not be marked as HttpOnly or Secure.
+     *
+     * @type {Object}
+     * @property {boolean} httpOnly - Indicates whether the cookie should be accessible only by the web server.
+     * @property {boolean} secure - Indicates whether the cookie should be sent only over a secure protocol (HTTPS).
+     */
     const options = {
       httpOnly: false,
       secure: false,
     };
 
+    /**
+     * Generates access and refresh tokens for the user with the provided user ID.
+     *
+     * @param {string} userId - The ID of the user to generate tokens for.
+     * @returns {Object} - An object containing the generated access and refresh tokens.
+     */
     const { accessToken, refreshToken } = await generateTokens(userId._id);
+
+    /**
+     * Refreshes the user's access and refresh tokens.
+     *
+     * @param {Object} res - The response object.
+     * @param {string} accessToken - The new access token to be set in the response.
+     * @param {string} refreshToken - The new refresh token to be set in the response.
+     * @param {Object} options - The options for setting the access and refresh tokens as cookies.
+     * @returns {Promise<Object>} - A response with the new access and refresh tokens.
+     * @throws {ApiError} - If the token is invalid or expired.
+     */
     return res
       .status(200)
       .cookie("accessToken", accessToken, options)
@@ -364,11 +538,19 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         )
       );
   } catch (error) {
+    /**
+     * Throws an API error with a 401 status code and the provided error message, or a default message if the error message is not available.
+     *
+     * @param {Error} error - The error object that triggered the API error.
+     * @throws {ApiError} - An API error with a 401 status code and the error message.
+     */
     throw new ApiError(401, error.message || "Invalid token found");
   }
 });
 
+// ---------------------------------------------------------------
 // TO CHANGE PASSWORD USER SHOULD LOGGEDIN BEFORE
+// ---------------------------------------------------------------
 /**
  * Changes the current user's password.
  *
@@ -437,27 +619,39 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const getCurrentUser = asyncHandler(async (req, res) => {
   // TODO: check if user is logged in
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
   // TODO: check if user is logged in
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const updateUserAvatar = asyncHandler(async (req, res) => {
   // TODO: check if user is logged in
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const updateUserCoverImage = asyncHandler(async (req, res) => {
   // TODO: check if user is logged in
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const getUserChannelProfile = asyncHandler(async (req, res) => {
   // TODO: check if user is logged in
 });
 
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
 const getWatchHistory = asyncHandler(async (req, res) => {
   // TODO: check if user is logged in
 });
